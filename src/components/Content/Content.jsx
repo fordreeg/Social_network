c6b0831f40a74eed45139a60913dbd5711e10487
vlad <fordreej@gmail.com>
2021-11-06 18:00:20 +0200

    added redirect and switch

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,6 +1,6 @@
 import style from './Content.module.css'
 import { Suspense, lazy } from 'react';
-import {Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Preloader from "../Common/Preloader/Preloader";
 
 const ProfileContainer = lazy(() => import('./Profile/ProfileContainer'));
@@ -12,18 +12,26 @@ const Content = (props) => {
     return (
         <div className={style.wrapper}>
             <Suspense fallback={<Preloader/>}>
-                <Route exact path='/profile/:userId?'>
-                    <ProfileContainer/>
-                </Route>
-                <Route exact path='/messages'>
-                    <MessagesContainer/>
-                </Route>
-                <Route exact path='/users'>
-                    <UsersContainer/>
-                </Route>
-                <Route path='/login'>
-                    <LoginContainer/>
-                </Route>
+                <Switch>
+                    <Route exact path='/'>
+                        <Redirect to={'/profile'}/>
+                    </Route>
+                    <Route exact path='/profile/:userId?'>
+                        <ProfileContainer/>
+                    </Route>
+                    <Route exact path='/messages'>
+                        <MessagesContainer/>
+                    </Route>
+                    <Route exact path='/users'>
+                        <UsersContainer/>
+                    </Route>
+                    <Route path='/login'>
+                        <LoginContainer/>
+                    </Route>
+                    <Route>
+                        <div>404 error</div>
+                    </Route>
+                </Switch>
             </Suspense>
         </div>
     )
