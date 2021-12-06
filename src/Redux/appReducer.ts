71844bbbc8bf03d1cbb6c063492b39e48dd6d0ab
vlad <fordreej@gmail.com>
2021-12-06 21:32:16 +0200

    added typeScript typing

--- src/Redux/appReducer.ts
+++ src/Redux/appReducer.ts
@@ -2,14 +2,6 @@ import {getAuthUserData} from "./authReducer";
 
 const INITIALIZED_SUCCESS:string = 'INITIALIZED_SUCCESS';
 
-<<<<<<< HEAD
-export type initialStateType = {initialized: boolean}
-const initialState: initialStateType = {
-    initialized: false,
-};
-
-const appReducer = (state = initialState, action: any):initialStateType => {
-=======
 export type initialStateType = {
     initialized: boolean,
 };
@@ -19,7 +11,6 @@ const initialState:initialStateType = {
 };
 
 const appReducer = (state = initialState, action:any):initialStateType => {
->>>>>>> 59673158118fced5652f0db5ae500730cc70bb70
     switch (action.type) {
         case INITIALIZED_SUCCESS:
             return {
@@ -32,20 +23,10 @@ const appReducer = (state = initialState, action:any):initialStateType => {
 };
 export default appReducer;
 
-<<<<<<< HEAD
 type initializedSuccessActionType = {type: typeof INITIALIZED_SUCCESS}
 export const initializedSuccess = ():initializedSuccessActionType => ({type: INITIALIZED_SUCCESS});
-=======
-export type initializedSuccessType = {type: typeof INITIALIZED_SUCCESS};
-export const initializedSuccess = ():initializedSuccessType => ({type: INITIALIZED_SUCCESS});
->>>>>>> 59673158118fced5652f0db5ae500730cc70bb70
 
 export const initializeApp = () => async (dispatch: any) => {
     await dispatch(getAuthUserData());
     dispatch(initializedSuccess())
-<<<<<<< HEAD
 }
-=======
-
-}
->>>>>>> 59673158118fced5652f0db5ae500730cc70bb70
